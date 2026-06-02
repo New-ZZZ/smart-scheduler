cacffbbe31917aa43c3e17a63bf6c1c36f0d67ab
New-ZZZ <2937907356@qq.com>
2026-06-02 12:59:40 +0800

    加入自动排班功能：输入员工姓名和轮班时间

--- README.md
+++ README.md
@@ -14,3 +14,4 @@ week1 process
 
 - setup complete
 - worker model created
+- "输入员工模型、轮班时间，自动化排班"
--- main.py
+++ main.py
@@ -4,9 +4,30 @@ def show_workers(workers):
     for worker in workers:
         print(worker)
 
-workers = ["Alice","Bob","Tom"]
-shifts = ["Day","Night"]
 
-for i in range(len(workers)):
-    shift = i % len(shifts)
-    print(workers[i],"->",shifts[shift])
+def assign_shift(workers,shifts):
+    for i in range(len(workers)):
+        shift = i % len(shifts)
+        print(f"{workers[i]}->{shifts[shift]}")
+
+# assign_shift(workers,shifts)
+
+workers = []
+shifts = []
+
+while(True):
+    worker_name = input("请输入员工姓名：")
+    if(worker_name=="0"):
+        print("员工姓名输入完毕!!!")
+        break
+    workers.append(worker_name)
+
+while(True):
+    shift_time = input("请输入轮班时间：")
+    if(shift_time=="0"):
+        print("轮班时间输入完毕!!!")
+        break
+    shifts.append(shift_time)
+
+assign_shift(workers,shifts)
+
